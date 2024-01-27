0c52984f87c7e0a7f14aa10348a1c1ffbe16a85a
Md.Sohan Mia <s20111102@bsfmstu.ac.bd>
2024-01-27 18:34:29 +0600

    Changes $ symble

--- action.php
+++ action.php
@@ -127,7 +127,7 @@ if (isset($_POST["getProduct"])) {
 									<div class='product-body'>
 										<p class='product-category'>$cat_name</p>
 										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
-										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>$990.00</del></h4>
+										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>Tk990.00</del></h4>
 										<div class='product-rating'>
 											<i class='fa fa-star'></i>
 											<i class='fa fa-star'></i>
@@ -191,7 +191,7 @@ if (isset($_POST["get_seleted_Category"]) || isset($_POST["selectBrand"]) || iss
 									<div class='product-body'>
 										<p class='product-category'>$cat_name</p>
 										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
-										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>$990.00</del></h4>
+										<h4 class='product-price header-cart-item-info'>$pro_price<del class='product-old-price'>Tk990.00</del></h4>
 										<div class='product-rating'>
 											<i class='fa fa-star'></i>
 											<i class='fa fa-star'></i>
@@ -327,7 +327,7 @@ if (isset($_POST["Common"])) {
 												</div>
 												<div class="product-body">
 													<h3 class="product-name"><a href="#">' . $product_title . '</a></h3>
-													<h4 class="product-price"><span class="qty">' . $n . '</span>$' . $product_price . '</h4>
+													<h4 class="product-price"><span class="qty">' . $n . '</span>Tk' . $product_price . '</h4>
 												</div>
 												
 											</div>';
@@ -335,7 +335,7 @@ if (isset($_POST["Common"])) {
 
 			echo '<div class="cart-summary">
 				    <small class="qty">' . $n . ' Item(s) selected</small>
-				    <h5>$' . $total_price . '</h5>
+				    <h5>Tk' . $total_price . '</h5>
 				</div>'
 ?>
 				
